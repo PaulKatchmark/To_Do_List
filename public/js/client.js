e4fc0b4632bc6864f4e10c1125fb1a10effeebf3
PaulKatchmark <paulkatchmark@gmail.com>
2016-10-15 19:52:30 -0500

    updated sql

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,38 @@
 $(function() {
-
-
+getTasks();
 
 
 
 });
+
+function getTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/todo',
+    success: displayTasks
+  });
+}
+
+function displayTasks(response) {
+  console.log(response);
+  var $list = $('#toDoList');
+  $list.empty();
+  response.forEach(function(task) {
+
+    var $li = $('<ol></ol>');
+    var $form = $('<form></form>');
+
+    var $compButton = $('<input type="checkbox" class="complete"></>');
+    $compButton.data('id', task.id);
+    $form.append($compButton);
+
+    $form.append('<p value="' + task.id + '">' + task.open_tasks + '</p>');
+
+    var $deleteButton = $('<button class="delete">Delete</button>');
+    $deleteButton.data('id', task.id);
+    $form.append($deleteButton);
+
+    $li.append($form);
+    $list.append($li);
+  });
+}
--- routes/todo.js
+++ routes/todo.js
@@ -7,7 +7,30 @@ var config = {
 
 var pool = new pg.Pool(config);
 
+router.get('/', function(req, res) {
+  pool.connect(function(err, client, done) {
+    try {
+      if(err) {
+        console.log('Error connecting to the DB', err);
+        res.sendStatus(500);
+        return;
+      }
+      client.query('SELECT * FROM tasks;', function(err, result) {
+      if(err) {
+        console.log('Error querying the DB', err);
+        res.sendStatus(500);
+        return;
+      }
 
+      console.log('Got rows from the DB:', result.rows);
+      res.send(result.rows);
+      });
+    } finally {
+      done();
+    }
+
+  });
+});
 
 
 
